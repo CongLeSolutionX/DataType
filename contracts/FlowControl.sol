c92eed6aac8aa69afe2f106da54ebf72ec3114e4
Cong Le <longchike@gmail.com>
2022-11-15 19:40:42 -0800

    - Create a Flow Control smart contract with 4 control flows

--- contracts/FlowControl.sol
+++ contracts/FlowControl.sol
@@ -2,24 +2,66 @@
 pragma solidity ^0.8.17;
 
 /*
-* @title Solidity data types
+* @title Solidity Flow Control
 * @author Cong Le
-* @notice A simply smart contract to demonstrate simple data types available in Solidity
+* @notice A simply smart contract to demonstrate flow controls in Solidity
 * 
 */
 
-contract DataTypes {
+contract FlowControl { 
+    function calcDozenDiscountIF(uint purchasedQty) private pure returns (bool) {
+        bool giveDozenPrize = false;
 
-    uint x = 9;
-    int i = -68; 
-    uint8 j = 17;
-    bool isEthereumCool = true; 
-    address owner = msg.sender; // msg.sender is the Ethereum address of the account that sent this transaction
-    bytes32 bMsg = "hello";
-    string sMsg = "hello"; 
-
-    function getStateVariables() public view returns (uint, int, uint, bool, address, bytes32, string memory) {
-        return (x, i, j, isEthereumCool, owner, bMsg, sMsg); 
+        if (purchasedQty >= 12)
+            giveDozenPrize = true;
+        else 
+            giveDozenPrize = false;
+        return (giveDozenPrize);
     }
 
+    // WHILE is an interation structure runs the body 0 or more time
+    function calcDozenDiscountWHILE(uint purchasedQty) private pure returns (bool) {
+        bool giveDozenPrice = false;
+        uint numDonuts = 1;
+
+        while (numDonuts < purchasedQty) { 
+            numDonuts++;
+            if (numDonuts >= 12) { 
+                giveDozenPrice = true;
+                break;
+                /* Need curly braces for if statement when the execution block inside the if statement has more than 1 line of code.
+                Otherwise, we dont need the curly brances if we only have 1 line of code in if statement. */
+            }
+        }
+        return (giveDozenPrice);
+    }
+
+    // DO-WHILE is an interation structure runs the body 1 or more time
+    function calcDozenDiscountDO(uint purchasedQty) private pure returns (bool) {
+        bool giveDozenPrice = false;
+        uint numDonuts = 1;
+
+        do { 
+            numDonuts++;
+            if (numDonuts >= 12) {
+                giveDozenPrice = true;
+                break;
+            }
+        } while (numDonuts < purchasedQty);
+
+        return (giveDozenPrice);
+    }
+
+    function calcDozenDiscountFOR(uint purchasedQty) private pure returns (bool) {
+        bool giveDozenPrice = false;
+        uint numDonuts = 1;
+
+        for (numDonuts = 1; numDonuts <= purchasedQty; numDonuts++) {
+            if (numDonuts >= 12) {
+                giveDozenPrice = true;
+                break;
+            }
+        }
+        return (giveDozenPrice);
+    }
 }
